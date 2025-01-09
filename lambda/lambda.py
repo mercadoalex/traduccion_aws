4e04d96c7b6e924b0a2bdb2c343a0fd033705c14
mercadoalex <mercadoalex@gmail.com>
2025-01-08 19:58:14 -0600

    fixing the buildspec

--- lambda/lambda.py
+++ lambda/lambda.py
@@ -1,6 +1,13 @@
 import re  # Import the regular expression module
+import logging
+
+# Configure logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def handler(event, context):
+    logger.info("Event: %s", event)
+    
     # Extract the request from the CloudFront event
     request = event['Records'][0]['cf']['request']
     
@@ -9,6 +16,8 @@ def handler(event, context):
     if viewerCountry:
         # Extract the country code from the 'accept-language' header
         countryCode = viewerCountry[0]['value']
+        logger.info("Country Code: %s", countryCode)
+        
         # Check if the country code starts with 'es' (for Spanish)
         if re.match(r'^es', countryCode):
             # Set the domain name to the Spanish assets bucket
@@ -16,6 +25,7 @@ def handler(event, context):
             request['origin']['s3']['domainName'] = domainName
             # Update the 'host' header to match the new domain name
             request['headers']['host'] = [{'key': 'host', 'value': domainName}]
+            logger.info("Updated domain name to Spanish assets bucket")
     
     # Return the modified request
     return request
